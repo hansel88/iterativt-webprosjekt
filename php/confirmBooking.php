30ce5c420d8626ef48166d8fa86261e72e552f1b
hansel88 <hpn_x@hotmail.com>
2015-03-12 12:20:03 +0100

    .

---
+++ php/confirmBooking.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'config.php';
+
+$token = $_GET['token'];
+
+if($token == "")
+{
+// no username entered
+echo "Something went wrong.";
+}
+else
+{
+
+}
--- php/dbSetup.php
+++ php/dbSetup.php
@@ -2,12 +2,12 @@
 require 'config.php';
 // Create the table: blog
 $sql = $database->prepare(
-	"CREATE TABLE room (room_nr varchar(255) NOT NULL, projector boolean NOT NULL, size int NOT NULL, PRIMARY KEY (room_nr));"
+	"CREATE TABLE IF NOT EXISTS room (room_nr varchar(255) NOT NULL, projector boolean NOT NULL, size int NOT NULL, PRIMARY KEY (room_nr));"
 );
 $sql->execute();
 // Create the table: comments
 $sql = $database->prepare(
-	"CREATE TABLE room_reservation (room_nr varchar(255) NOT NULL,user_email varchar(255) NOT NULL, fromDate datetime NOT NULL, toDate datetime NOT NULL, token varchar(255) NOT NULL, PRIMARY KEY (room_nr, token), FOREIGN KEY (room_nr) REFERENCES room(room_nr));"
+	"CREATE TABLE IF NOT EXISTS room_reservation (room_nr varchar(255) NOT NULL,user_email varchar(255) NOT NULL, fromDate datetime NOT NULL, toDate datetime NOT NULL, token varchar(255) NOT NULL, PRIMARY KEY (room_nr, token), FOREIGN KEY (room_nr) REFERENCES room(room_nr));"
 );
 $sql->execute();
 // Add some example content to the database
