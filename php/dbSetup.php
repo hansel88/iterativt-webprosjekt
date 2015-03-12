51f05f1d5e75d74c909c4cb4c17684cc7afe86a3
hansel88 <hpn_x@hotmail.com>
2015-03-12 11:33:37 +0100

    database setup fungerer

--- php/dbSetup.php
+++ php/dbSetup.php
@@ -7,7 +7,7 @@ $sql = $database->prepare(
 $sql->execute();
 // Create the table: comments
 $sql = $database->prepare(
-	"CREATE TABLE room_reservation (room_nr varchar(255) NOT NULL,user_email varchar(255) NOT NULL, fromDate datetime NOT NULL, toDate datetime NOT NULL, token varchar(255) NOT NULL, PRIMARY KEY (id, token), FOREIGN KEY (room_nr) REFERENCES room(room_nr));"
+	"CREATE TABLE room_reservation (room_nr varchar(255) NOT NULL,user_email varchar(255) NOT NULL, fromDate datetime NOT NULL, toDate datetime NOT NULL, token varchar(255) NOT NULL, PRIMARY KEY (room_nr, token), FOREIGN KEY (room_nr) REFERENCES room(room_nr));"
 );
 $sql->execute();
 // Add some example content to the database
@@ -15,6 +15,15 @@ $sql->execute();
 
 $sql = $database->prepare(
 	"INSERT INTO room (room_nr, projector, size) VALUES (:room_nr, :projector, :size);"
+);
+$sql->execute(array(
+	'room_nr' => "Rom 24",
+	'projector' => true,
+	'size' => 3
+));
+
+$sql = $database->prepare(
+	"INSERT INTO room_reservation (room_nr, projector, size) VALUES (:room_nr, :projector, :size);"
 );
 $sql->execute(array(
 	'room_nr' => "Rom 24",
