8f41f3c1d54f5bb6a744803f16d15361e6916e93
hansel88 <hpn_x@hotmail.com>
2015-03-12 12:40:01 +0100

    confirmBooking.php added, + added confirmed field on room_reervation table

--- php/confirmBooking.php
+++ php/confirmBooking.php
@@ -7,9 +7,25 @@ $token = $_GET['token'];
 if($token == "")
 {
 // no username entered
-echo "Something went wrong.";
+echo "Noe gikk galt.";
 }
 else
 {
+	$sql = $database->prepare("select * from room_reservation where generatedToken = :token");
+	$sql->setFetchMode(PDO::FETCH_OBJ);
+	$sql->execute(array(
+		'generatedToken' => $token
+	));
 
-}
+	if (!$sql->fetch())
+	{
+		echo '<p>Vi kunne dessverre ikke finne reservasjonen din.</p>';
+	}
+	else
+	{
+		$reservation = $sql.fetch();
+		$sql = $database->prepare("update table from room_reservation set confirmed = true where generatedToken = :token");
+		$sql->execute();
+
+		echo '<p>Reservasjonen din er bekreftet</p>';
+	}
