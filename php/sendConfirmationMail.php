1b3f251ab202d9de5da9e6d6e8c6bee4bcc934c5
h3rmanj <hjanimations@hotmail.com>
2015-03-13 12:00:50 +0100

    fastbugfix

--- php/sendConfirmationMail.php
+++ php/sendConfirmationMail.php
@@ -3,7 +3,7 @@ require 'config.php';
 require 'header.php';
 
 // Sjekker om alt er fylt ut som det skal
-if(!isset($_SESSION['mail'], $_POST['room'], $_SESSION['fromDate'], $_SESSION['toDate']) {
+if(!isset($_SESSION['mail'], $_POST['room'], $_SESSION['fromDate'], $_SESSION['toDate'])) {
     http_response_code(400); // 400 bad request
     exit();
 }
@@ -44,7 +44,7 @@ else {
     }
 }
 
-echo '<section id=wrapper><h1>Rom satt av</h1><p>Rommet er nå satt av, for å fullføre bestillingen må du følge instruksjonene fått på mail.</p></submit>'
+echo '<section id=wrapper><h1>Rom satt av</h1><p>Rommet er nå satt av, for å fullføre bestillingen må du følge instruksjonene fått på mail.</p></submit>';
 require 'footer.php';
 session_destroy();
 ?>
